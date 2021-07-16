d3aebf185ff35982823dfd5fe305cdc904a35822
unknown <vladyslavvasylenko03@gmail.com>
2021-07-16 15:33:12 +0300

    block footer

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 'use strict';
 
-$(document).ready(function(){
-  $(".search-btn").click(function(){
-    $(".search-wrap, .search-input").toggleClass("active");
-    $(".search-input").focus();
-  });
-});
+// $(document).ready(function(){
+//   $(".search-btn").click(function(){
+//     $(".search-wrap, .search-input").toggleClass("active");
+//     $(".search-input").focus();
+//   });
+// });
+
